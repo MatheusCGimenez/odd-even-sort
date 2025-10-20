632a4fd5bdee8eb17364afbe7d56e337ea53ceb0
Matheus Gimenez <161307822+MatheusCGimenez@users.noreply.github.com>
2025-10-20 02:09:37 -0400

    code updated

--- odd-even-sort.c
+++ odd-even-sort.c
@@ -1,54 +1,59 @@
 #include <stdio.h>
+
 #include <stdbool.h>
 
 void trocar(int* a, int* b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+  int temp = *a;
+  *a = *b;
+  *b = temp;
 }
 
 void oddEvenSort(int arr[], int n) {
-    bool estaOrdenado = false;
+  bool estaOrdenado = false;
 
-    while (!estaOrdenado) {
-        estaOrdenado = true;
+  while (!estaOrdenado) {
+    estaOrdenado = true;
 
-        // compara e troca os elementos em posições ímpares (1-2, 3-4, 5-6, ...)
-        for (int i = 1; i <= n - 2; i = i + 2) {
-            if (arr[i] > arr[i + 1]) {
-                trocar(&arr[i], &arr[i + 1]);
-                estaOrdenado = false;
-            }
-        }
+    // odd-even (ímpar-par)
+    for (int i = 1; i <= n - 2; i = i + 2) {
+      if (arr[i] > arr[i + 1]) {
+        trocar(&arr[i], &arr[i + 1]);
 
-        // compara e troca os elementos em posições pares (0-1, 2-3, 4-5, ...)
-        for (int i = 0; i <= n - 2; i = i + 2) { 
-            if (arr[i] > arr[i + 1]) {
-                trocar(&arr[i], &arr[i + 1]);
-                estaOrdenado = false;
-            }
-        }
+        estaOrdenado = false;
+      }
     }
+
+
+    // even-odd (par-ímpar  )
+    for (int i = 0; i <= n - 2; i = i + 2) {
+      if (arr[i] > arr[i + 1]) {
+        trocar(&arr[i], &arr[i + 1]);
+
+        estaOrdenado = false;
+      }
+    }
+  }
 }
 
 void imprimirArray(int arr[], int tamanho) {
-    for (int i = 0; i < tamanho; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
+  for (int i = 0; i < tamanho; i++) {
+    printf("%d ", arr[i]);
+  }
+
+  printf("\n");
 }
 
 int main() {
-    int numeros[] = { 34, 2, 10, -9, 23, 0, 1 };
-    int tamanho = 7;
+  int numeros[] = {34, 2, 10, -9, 23, 0, 1};
+  int tamanho = 7;
 
-    printf("Fila bagunçada: ");
-    imprimirArray(numeros, tamanho);
+  printf("Fila bagunçada: ");
+  imprimirArray(numeros, tamanho);
 
-    oddEvenSort(numeros, tamanho);
+  oddEvenSort(numeros, tamanho);
 
-    printf("Fila organizada:  ");
-    imprimirArray(numeros, tamanho);
+  printf("Fila organizada:  ");
+  imprimirArray(numeros, tamanho);
 
-    return 0;
+  return 0;
 }
